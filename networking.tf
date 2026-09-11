983b2f97c37f9317d7dd2706289d0631035d08f2
Ferdinand Ferdinand <ferdinandf@fida.de>
2026-09-11 20:22:14 +0200

    subnet added

--- networking.tf
+++ networking.tf
@@ -1,3 +1,13 @@
 resource "aws_vpc" "tf_cloud" {
   cidr_block = var.vpc_cidr
+  tags = {
+    Name = "terraform-cloud"
+  }
+}
+resource "aws_subnet" "tf_cloud" {
+  vpc_id     = aws_vpc.tf_cloud.id
+  cidr_block = var.subnet_cidr
+  tags = {
+    Name = "terraform-cloud"
+  }
 }
